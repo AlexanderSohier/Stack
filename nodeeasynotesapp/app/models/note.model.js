0f93c9daa44699a42b8b8a9402f86126fbbfd6cf
Alexander Sohier <alex4694@live.fr>
2018-03-14 11:37:25 +0100

    nettoyage

--- nodeeasynotesapp/app/models/note.model.js
+++ nodeeasynotesapp/app/models/note.model.js
@@ -1,5 +1,8 @@
 var mongoose = require('mongoose');
 
+/*schema necessaire pour structurer les données et requis par mongoose,
+contient les champs constituant une note.
+le timestamps:true sert à générer des champs contenant les timestamps de création et de modification*/
 var NoteSchema = mongoose.Schema({
     title: String,
     content: String
